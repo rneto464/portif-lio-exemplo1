7cabb568daf0c7e26ec464a6d4888c4f02b0757c
rneto464 <108296848+rneto464@users.noreply.github.com>
2024-02-11 14:03:13 -0300

    att

---
+++ efeito.js
@@ -0,0 +1,141 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+//topo do site//
+
+revelar.reveal('.efeito-txt-topo' ,
+{
+    duration: 2000,
+    distance: '90px' 
+})
+
+revelar.reveal('.efeito-img-topo' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500 
+})
+
+
+//titulos das seções//
+
+revelar.reveal('.titulo' ,
+{
+    duration: 2000,
+    distance: '90px'
+   
+})
+
+//especialidades//
+
+revelar.reveal('.efeito-e1' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+   
+})
+
+revelar.reveal('.efeito-e2' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay:1000
+   
+})
+
+revelar.reveal('.efeito-e3' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay:1500
+   
+})
+
+//sobre//
+
+
+revelar.reveal('.efeito-img-sobre' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+   
+})
+
+
+revelar.reveal('.efeito-txt-sobre' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'right'
+   
+})
+
+//MEU PORTIFOLIO//
+
+revelar.reveal('.img-efeito-1' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+   
+})
+
+revelar.reveal('.img-efeito-2' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+   
+})
+
+revelar.reveal('.img-efeito-3' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+   
+})
+
+//FORMULARIO//
+
+revelar.reveal('.form-efeito1' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'bottom'
+   
+})
+
+revelar.reveal('.form-efeito2' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'bottom'
+   
+})
+
+revelar.reveal('.form-efeito3' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500,
+    origin: 'bottom'
+   
+})
+
+revelar.reveal('.form-efeito4' ,
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 2000,
+    origin: 'bottom'
+   
+})
+
+
